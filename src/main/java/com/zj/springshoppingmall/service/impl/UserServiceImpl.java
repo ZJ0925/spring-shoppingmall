23ba901e019e054014221899f58f4821fa2a1bcd
YZJ <yzjjj0925@gmail.com>
2025-01-08 14:31:04 +0800

    優化密碼安全功能： 使用BCrypt替代掉原來的Hash，提升密碼安全性 (bcrypt演算法自帶鹽值產生機制，並且在每次加密時都能產生不同的雜湊值，因此即使相同的密碼，每次產生的雜湊值也不同，提高了密碼的安全性。)

--- src/main/java/com/zj/springshoppingmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/zj/springshoppingmall/service/impl/UserServiceImpl.java
@@ -8,11 +8,12 @@ import com.zj.springshoppingmall.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -30,6 +31,9 @@ public class UserServiceImpl implements UserService {
     //註冊帳號功能
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //使用bcrypt加密
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
         //檢查註冊的email(透過user去接住getUserByEmail的值，為String)
         User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
         //如果user不是null的話(表示已經註冊過)
@@ -38,11 +42,15 @@ public class UserServiceImpl implements UserService {
             //拋出Response狀態異常(BAD_REQUEST， HttpStatus = 400)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        //使用MD5生成密碼雜湊值(已被彩虹表破解，反制方式為加鹽(Salt)強化Hash安全性)
-        //將前端傳進來的參數轉換為byte後，使用DigestUtils裡的md5DigestAsHex方法在轉換為哈希
+        /*
+        使用MD5生成密碼雜湊值(已被彩虹表破解，反制方式為加鹽(Salt)強化Hash安全性)
+        將前端傳進來的參數轉換為byte後，使用DigestUtils裡的md5DigestAsHex方法在轉換為哈希
         String hashPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
-        //將userRegisterRequest裡的密碼透過set的方式替換成hashPassword
-        userRegisterRequest.setPassword(hashPassword);
+         */
+        //使用使用bcrypt對前端傳進來的註冊密碼進行加密
+        String encodedPassword = passwordEncoder.encode(userRegisterRequest.getPassword());
+        //將userRegisterRequest裡的密碼透過set的方式替換成encodedPassword
+        userRegisterRequest.setPassword(encodedPassword);
         //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -50,6 +58,9 @@ public class UserServiceImpl implements UserService {
     //登入功能
     @Override
     public User login(UserLoginRequest userLoginRequest) {
+        //使用bcrypt加密
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
         //先使用Dao層的getUserByEmail查詢資料庫中有無email的存在
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
         if(user == null){
@@ -57,11 +68,11 @@ public class UserServiceImpl implements UserService {
             //強制停止前端請求(BAD_REQUEST，http狀態碼為400)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        //將前端輸入的密碼轉換為哈希密碼
-        String hashPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
         //判斷流程須於service實作
-        //String類型的值必須使用equal來做比較
-        if(user.getPassword().equals(hashPassword)){
+        //String類型的值必須使用equal來做比較()
+        //BCryptPasswordEncoder的matches(原密碼,加密後的密碼)
+        if(passwordEncoder.matches(userLoginRequest.getPassword(), user.getPassword())){
             return user;
         }else{
             log.warn("{}這組email的密碼不正確",userLoginRequest.getEmail());
